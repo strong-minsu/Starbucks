37557df3d62ec98f9ca8bf9d1319c08a6a2c55fc
strong-minsu <dlalstn5789@gmail.com>
2022-08-04 17:46:01 +0900

    common module separation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", function(){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", function(){
-  searchEl.classList.add("focused");
-  // 속성 지정
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener("blur", function(){
-  searchEl.classList.remove("focused");
-  // 속성 지정
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector("#to-top");
 window.addEventListener('scroll', _.throttle(function (){
@@ -152,6 +132,4 @@ spyEls.forEach(function(spyEl){
     .addTo(new ScrollMagic.Controller()); 
 });
 
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();  //2022로 반환됨
 
